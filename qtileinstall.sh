a5dd4806562cfddca3864d0e7b120ec3b0ea27c6
nat35lint <nahtenolird@proton.me>
2022-06-14 18:46:00 +0800

    qtilescript

--- qtileinstall.sh
+++ qtileinstall.sh
@@ -2,13 +2,53 @@
 
 cd
 
+clear 
+
+echo " 
+
+
+this script will overwrite your qtile picom kitty config.
+       
+
+backup first then relaunch the script again to cancel the script press [CTRL+C].
+
+but if you are dumb enough that you accidentally proceeded dont worry
+
+you files will be backed up but its name will change theyre names will be
+
+[qtile.bak kitty.bak picom.bak] 
+
+in your ~/.configs
+
+
+"
+
+
+cd ~/.config/
+
+mv picom picom.bak
+
+cd
+
+cd ~/.config/
+
+mv qtile qtile.bak
+
+cd
+
+cd ~/.config
+
+mv kitty kitty.bak
+
+cd
+
 cd ~/dotfilespb
 
-mv picom ~/.config/
+cp -r picom ~/.config/
 
-mv kitty ~/.config/
+cp -r kitty ~/.config/
 
-mv qtile ~/.config/
+cp -r qtile ~/.config/
 
 yay -S python python-psutil qtile picom-jonaburg-git kitty
 
